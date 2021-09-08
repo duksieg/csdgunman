58a73c66fc40a123580b9e21f40de9cf027b6786
duksieg <duksieg41826@hotmail.com>
2021-09-08 20:27:13 +0700

    add bootstrap all

--- src/components/topNav.js
+++ src/components/topNav.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container, Nav, Navbar, Image } from "react-bootstrap";
 import {  Facebook } from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 
 const TopNav = () => {
